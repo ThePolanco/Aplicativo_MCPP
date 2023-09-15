5051d50c1892e49e18bb7d27dfd5b38675d17d7c
ThePolanco <polancojeison21@gmail.com>
2023-09-15 16:00:38 -0500

    Project prediction page update
    UPDATE

---
+++ src/app.py
@@ -0,0 +1,18 @@
+# Importar Flask y request
+from flask import Flask, render_template, request, redirect, url_for
+
+# Creación de la aplicación
+app = Flask(__name__)
+
+# Ruta para el index
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Control del error 404
+def error_404(error):
+    return render_template('error_404.html'), 404
+
+if __name__ == '__main__':
+    app.register_error_handler(404, error_404)
+    app.run(debug = True, port = 2023)
