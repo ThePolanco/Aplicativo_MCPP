9d48c032c4bc2ae5f06450a089d930e39b316fba
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-09-28 01:27:40 -0500

    Eliminar fila de un csv en la lectura de este

--- Prueba_CSV_Desde_Mongo/csvMongo.py
+++ Prueba_CSV_Desde_Mongo/csvMongo.py
@@ -5,6 +5,36 @@ import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime, time
 
+
+
+# Importar Flask y request
+from flask import Flask, render_template, request, redirect, url_for
+#
+
+
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+# Separar conjunto de entrenamiento y de validacion
+from sklearn.model_selection import train_test_split
+# se empiezan a construir los clasificadores con arboles de decisión 
+from sklearn.tree import DecisionTreeClassifier
+# Utilizaremos random forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from matplotlib import colors
+from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_selection import SelectKBest
+
+
+
+
+
 #pip install openpyxl
 
 
@@ -19,6 +49,8 @@ df = pd.DataFrame(list(collection.find()))
 df.drop('fecha', inplace=True, axis=1)
 df.drop('hora', inplace=True, axis=1)
 df.drop('_id', inplace=True, axis=1)
+
+
 '''
 # Obtener un df con datos filtrados: (precio < 50)
 df = pd.DataFrame(list(collection.find({"price": {"$lt": 50}})))
@@ -30,3 +62,12 @@ df = pd.DataFrame(list(collection.find({"date": { "$gt" : midnight}}, {"_id": 0}
 '''
 ## Exportar a CSV:
 df.to_csv('filename.csv', index=False)
+
+#**************************************************************************************************************************************************************
+#leer archivo quittando lasa primeras filas
+ddaa=pd.read_csv("C:/Users/Usuario/Desktop/Tesis/Proyecto_de_Grado/filename.csv", sep=',', header=None)
+
+
+X = ddaa.iloc[1:,:-1]
+
+print(X)
